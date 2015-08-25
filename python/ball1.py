d9b10fa9489c23a768aea0f46092978b73d2c72a
camillanore <camilla.nore@gmail.com>
2015-08-25 16:19:50 +0200

    Oppgave 1.11 air_resistance

--- python/ball1.py
+++ python/ball1.py
@@ -49,3 +49,45 @@ g = 9.81  # acceleration of gravity
 t = 0.6   # time
 y = v0*t - 0.5*g*t**2  # vertical position
 print y
+
+a = 1      # 1st statement (assignment statement)
+b = 2      # 2nd statement (assignment statement)
+c = a + b  # 3rd statement (assignment statement)
+print c    # 4th statement (print statement)
+
+import numpy as np
+import math
+import pylab
+# Exercise 1.10: Calculate the gaussian
+
+def gaussian(x, m=0, s=2):
+    f = 1/math.sqrt(2*np.pi*s) * math.exp(-.5*((x-m)/s)**2.0)
+    return f
+
+print gaussian(1)
+
+print gaussian(x=1, m=3, s=1)
+ 
+# Exercise 1.11: Compute the air resistance on a football
+
+def air_resistance(velocity,           #m/s
+                   radius=0.11,         #m
+                   drag_coefficient=0.2):   #no units
+        # navngi konstante variable med k            
+    k_air_density = 1.2 # kg/m**3
+    area = np.pi*radius**2
+    drag_force = (0.5*drag_coefficient*k_air_density*
+                        area*velocity**2)
+    return drag_force # N
+    #del an funk
+#ikke del av funk
+print air_resistance(velocity=4)
+
+air_drag=[]
+velocity=[]
+for i in range(0,400):
+    velocity.append(i/10.0)
+    air_drag.append(air_resistance(i/10.0))
+pylab.plot(velocity,air_drag)
+pylab.xlabel('Velocity, m/s')
+pylab.ylabel('Drag force, N')

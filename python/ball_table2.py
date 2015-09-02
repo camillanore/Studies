83d80c7ce78c6143c88376bbbb91aa277c740983
camillanore <camilla.nore@gmail.com>
2015-09-02 10:53:20 +0200

    INF1100_Delivery#2

---
+++ python/ball_table2.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 01 14:45:19 2015
+
+@author: Camilla Nore //camilnor
+"""
+
+#Exercise 2.8: Store values from a formula in lists
+#This exercise aims to produce the same table of numbers as in Exercise 2.7,
+#but with different code. First, store the t and y values in two lists t and
+#y. Thereafter, write out a nicely formatted table by traversing the two
+#lists with a for loop.
+#Hint. In the for loop, use either zip to traverse the two lists in parallel,
+#or use an index and the range construction.
+#Filename: ball_table2.py.
+#Ex2.7y(t) = v0t − 1/2*gt^2
+# Use n + 1 uniformly spaced t values throughout the interval [0, 2v0/g].
+# use a for loop to produce the table
+
+t=[0,2V*g]
+
+for t in y
+    range(0,1)
+    y(t) = [v*t − 0.5*g*t^2]
+    print t, y 
---
+++ python/sum_while.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Sep 01 15:22:23 2015
+
+@author: NBCNO1
+"""
+# Task: s=sum(1/k) for k=1 to M=100
+#Explain why the following program does not work
+#s = 0; k = 1; M = 100
+#while k < M:
+#s += 1/k
+#print s
+
+k = 1.0
+s = 0
+M = 100
+
+# You can write this loop in two ways, using while:
+while k < M:
+    s += 1.0/k
+    k += 1
+print 'While loop, Sum of 1/n: ', s
+
+# Or using for:
+
+s2 = 0.0
+for k in range(1,M):
+    s2 += 1.0/k
+print 'For loop, Sum of 1/n: ', s2
+
+

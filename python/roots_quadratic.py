774b93fd4af30467babe1b4b4e1f909413ad0a87
camillanore <camilla.nore@gmail.com>
2015-09-09 23:10:54 +0200

    Delivery#3_INF1100 Nosetest

--- python/roots_quadratic.py
+++ python/roots_quadratic.py
@@ -33,13 +33,20 @@ import numpy as np
 import numpy.lib.scimath as npsci
 
 def main():
-    print "Running Roots Quadratic"
+    print "Running Roots Quadratic, to test, please use:"
+    print "nosetests roots_quadratic.py -v -s"
+    print "---"
+    print "Example, the roots of x^2 - 2x +2 =0 is:"
+    print roots(1,-2,2)
+    print "..."
+    print "Running tests from main function:"
     test_sqrt()
     print type(npsci.sqrt(-1) + 1)
     print type(5 +1j)
     print roots(1,2,3)
     tests_roots_complex()
     tests_roots_real()
+    print "All tests passed. BOOM!"
     
 def roots(a, b, c):
     """Find the roots and solution to the equation on the form
@@ -60,6 +67,7 @@ def test_sqrt():
     assert npsci.sqrt(-1)==1j, 'sqrt not working as suspected'
     
 def tests_roots_real():
+    """Test with real roots"""
     r1 = 1.0
     r2 = 1.0
    # x^2 - (r1+r2)x + r1*r2 = 0
@@ -68,17 +76,16 @@ def tests_roots_real():
     b = -(r1 + r2)
     c = r1*r2
     result = roots(a, b, c)
-    print 'Test real roots: (a,b,c) --> (x1,x2):'
+    print '(a,b,c) --> (x1,x2):'
     print (a,b,c), '-->', result
     #Warning: The order is important, let r1 be the biggest root.
     assert result == (r1, r2), ('Expected:' + str((r1,r2)) +
-                                'Result is: ' + str(result))
-    
-if __name__=='__main__':
-    """If this python script is run directly"""
-    main()    
+                                'Result is: ' + str(result))  
+    assert isinstance(result[0], float), 'Expected float root'
+    assert isinstance(result[1], float), 'Expected float root'
 
 def tests_roots_complex():
+    """Test with complex roots"""
     r1 = 1.0 + 1.0j
     r2 = 1.0 - 1.0j
    # x^2 - (r1+r2)x + r1*r2 = 0
@@ -87,16 +94,57 @@ def tests_roots_complex():
     b = -(r1 + r2)
     c = r1*r2
     result = roots(a, b, c)
-    print 'Test complex roots: (a,b,c) --> (x1,x2):'
+    print '(a,b,c) --> (x1,x2):'
     print (a,b,c), '-->', result
     print type(result[0])
-    assert False, 'Test has not been implemented' # TODO
     assert result == (r1, r2), ('Expected:' + str((r1,r2)) +
                                 'Result is: ' + str(result))
-    assert isinstance(result[0], complex)
-"""
+    assert isinstance(result[0], complex), 'Expected complex root'
+    assert isinstance(result[1], complex), 'Expected complex root'
+# NB!: The main shall always be the last
+if __name__=='__main__':
+    """If this python script is run directly"""
+    main()  
 
+"""
+Example from running as script:
+
+----------------------------------------------------------------------
+Ran 3 tests in 0.019s
+
+OK
+
+Running Roots Quadratic
+
+C:\Users\NBCNO1\Documents\Studies\python>nosetests roots_quadratic.py -s -v
+roots_quadratic.test_sqrt ... ok
+
+roots_quadratic.tests_roots_real ... Test real roots: (a,b,c) --> (x1,x2):
+(1, -2.0, 1.0) --> (1.0, 1.0)
+ok
+roots_quadratic.tests_roots_complex ... Test complex roots: (a,b,c) --> (x1,x2):
+(1, (-2-0j), (2+0j)) --> ((1+1j), (1-1j))
+<type 'numpy.complex128'>
+ok
+
+Example from running in python:
+
+'C:/Users/NBCNO1/Documents/Studies/python')
+Running Roots Quadratic, to test, please use:
+nosetests roots_quadratic.py -v -s
+---
+Example, the roots of x^2 - 2x +2 =0 is:
+((1+1j), (1-1j))
+...
+Running tests from main function:
 <type 'numpy.complex128'>
 <type 'complex'>
 ((-1+1.4142135623730951j), (-1-1.4142135623730951j))
+(a,b,c) --> (x1,x2):
+(1, (-2-0j), (2+0j)) --> ((1+1j), (1-1j))
+<type 'numpy.complex128'>
+(a,b,c) --> (x1,x2):
+(1, -2.0, 1.0) --> (1.0, 1.0)
+All tests passed. BOOM!
+
 """

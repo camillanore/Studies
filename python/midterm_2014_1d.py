42a43c38a8cab2cf776925b4150e6e9e67543bd4
camillanore <camilla.nore@gmail.com>
2015-10-03 20:43:51 +0200

    Eksamen 2014

---
+++ python/midterm_2014_1d.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 03 17:47:41 2015
+
+@author: NBCNO1
+"""
+
+import sys
+a = 4
+b = 6
+c = a + b
+print a + b
+a = float(sys.argv[1])
+b = float(sys.argv[2])
+d = a + b
+print c
+print d
+print a
+print b
+print a + b
+print type(b)
+print sys.argv

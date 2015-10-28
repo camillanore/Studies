045ccae787e0bad1be20128456c17a92a0094a10
camillanore <camilla.nore@gmail.com>
2015-10-28 20:35:36 +0100

    Linalg oblig 2

---
+++ python/f2c_shortcut_plot.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct 11 22:11:14 2015
+
+@author: NBCNO1
+"""
+
+"""
+Exercise 5.12: Plot exact and inexact Fahrenheit-Celsius
+conversion formulas
+A simple rule to quickly compute the Celsius temperature from the
+Fahrenheit degrees is to subtract 30 and then divide by 2: C = (F −30)/2.
+Compare this curve against the exact curve C = (F − 32)5/9 in a plot.
+Let F vary between −20 and 120. Filename: f2c_shortcut_plot.py.
+
+"""
